88181efb4ab783a872da58a618fd90619007d468
chous <chous@acm-sl.org>
2015-02-24 17:00:50 +0100

    Project setup and first test of the Java8 parser.

---
+++ src/test/java/org/acmsl/javacss/java8/parser/Java8ParserTest.java
@@ -0,0 +1,31 @@
+package org.acmsl.javacss.java8.parser;
+
+import org.junit.Test;
+import org.junit.Assert;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+@RunWith(JUnit4.class)
+public class Java8ParserTest {
+    @Test public void can_parse_an_interface_with_extends_and_a_single_method()
+        throws Exception {
+        String input =
+              "public interface Resolver\n"
+            + "    extends Serializable {\n\n"
+
+            + "    public int resolve(String value);\n"
+            + "}\n";
+            
+        Java8Lexer lexer =new Java8Lexer(new ANTLRInputStream(input));
+
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        Java8Parser parser = new Java8Parser(tokens);
+        ParseTree ast = parser.compilationUnit();
+        Assert.assertNotNull(ast);
+    }
+}
